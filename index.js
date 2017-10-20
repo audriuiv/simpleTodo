8de6ecb94c0c3715ad74ce712ff42197ccf3fb58
Audra <audriui.v@gmail.com>
2017-10-21 00:07:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import configureStore from './store/configureStore'
+import FilterLink from './containers/FilterLink'
 
 const store = configureStore()
 
